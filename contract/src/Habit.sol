77b55606d3ab77b333d638a6d5f85bcd5492f0da
Son <sitthavee.t@gmail.com>
2024-08-24 22:53:16 +0700

    add test case for smart contract

--- contract/src/Habit.sol
+++ contract/src/Habit.sol
@@ -62,6 +62,8 @@ contract HabitContract {
 
     function sponsorPledge(uint pledgeId) external payable {
         require(msg.value > 0, "Sponsor amount must be greater than 0");
+        require(pledgeId < pledgeList.length, "Invalid pledge ID");
+        require(!pledgeList[pledgeId].isRedeemed, "Pledge has already been redeemed");
 
         Sponsor memory newSponsor = Sponsor({
             sponsorAddress: msg.sender,
@@ -70,6 +72,12 @@ contract HabitContract {
         });
         sponsorList.push(newSponsor);
         pledgeAmount[pledgeId] += msg.value;
+
+        // add to pledge sponsor amount
+        Pledge storage pledge = pledgeList[pledgeId];
+        pledge.sponsorAmount += msg.value;
+
+        // emit event
         emit SponsorCreated(sponsorList.length - 1);
     }
 
@@ -79,9 +87,13 @@ contract HabitContract {
         require(pledgeList[pledgeId].endDate < block.timestamp, "Pledge is ongoing");
         require(successDays <= pledgeList[pledgeId].totalDays, "Success days must be less than or equal to total days");
 
+        // mark the pledge as redeemed
+        pledgeList[pledgeId].isRedeemed = true;
+
         // if they success every single day, transfer the total amount to the owner + sponsor amount
         if (successDays == pledgeList[pledgeId].totalDays) {
-            payable(msg.sender).transfer(pledgeAmount[pledgeId]);
+            (bool success, ) = (msg.sender).call{value: pledgeAmount[pledgeId]}("");
+            require(success, "Transfer failed");
 
             // emit event
             emit PledgeRedeemed(pledgeId, pledgeAmount[pledgeId]);
@@ -92,21 +104,23 @@ contract HabitContract {
             // return all the sponsored money in this pledgeId to the sponsors
             for (uint i = 0; i < sponsorList.length; i++) {
                 if (sponsorList[i].pledgeId == pledgeId) {
-                    payable(sponsorList[i].sponsorAddress).transfer(sponsorList[i].sponsorAmount);
+                    (bool success, ) = (sponsorList[i].sponsorAddress).call{value: sponsorList[i].sponsorAmount}("");
+                    require(success, "Transfer to sponsor failed");
                 }
             }
             // return some amount to the owner address
-            uint returnAmount = pledgeList[pledgeId].betAmount * successDays / pledgeList[pledgeId].totalDays;
-            payable(msg.sender).transfer(returnAmount);
+            uint returnAmount = (pledgeList[pledgeId].betAmount * successDays) / pledgeList[pledgeId].totalDays;
+            (bool success1, ) = (msg.sender).call{value: returnAmount}("");
+            require(success1, "Transfer to owner failed");
 
-            // take some amount to the deployer
-            payable(deployerAddress).transfer(pledgeList[pledgeId].betAmount - returnAmount);
+            // to donate to charity (in the future)
+            uint donateCharityAmount = pledgeList[pledgeId].betAmount - returnAmount;
+            (bool success2, ) = (deployerAddress).call{value: donateCharityAmount}("");
+            require(success2, "Transfer to deployer failed");
 
             // emit event
             emit PledgeRedeemed(pledgeId, returnAmount);
         }
-        // mark the pledge as redeemed
-        pledgeList[pledgeId].isRedeemed = true;
         
     }
 
--- contract/test/Habit.t.sol
+++ contract/test/Habit.t.sol
@@ -1,3 +1,99 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
 
+import "forge-std/Test.sol";
+import "forge-std/console.sol";
+import "../src/Habit.sol";
+
+contract HabitTest is Test {
+    HabitContract habit;
+    address owner;
+    address sponsor;
+    address deployer;
+
+    function setUp() public {
+        deployer = address(this);
+        owner = address(0x1);
+        sponsor = address(0x2);
+        vm.prank(deployer);
+        habit = new HabitContract();
+    }
+
+    function testCreateNewPledge() public {
+        vm.prank(owner);
+        vm.deal(owner, 1 ether);
+        uint16 totalDays = 30;
+        habit.createNewPledge{value: 1 ether}(totalDays);
+
+        (address pledgeOwner, uint endDate, uint betAmount, , uint16 pledgeTotalDays, uint16 successDays, bool isRedeemed) = habit.pledgeList(0);
+        
+        assertEq(pledgeOwner, owner);
+        assertEq(betAmount, 1 ether);
+        assertEq(pledgeTotalDays, totalDays);
+        assertEq(successDays, 0);
+        assertEq(isRedeemed, false);
+    }
+
+    function testSponsorPledge() public {
+        vm.prank(owner);
+        vm.deal(owner, 1 ether);
+        habit.createNewPledge{value: 1 ether}(30);
+
+        vm.prank(sponsor);
+        vm.deal(sponsor, 0.5 ether);
+        habit.sponsorPledge{value: 0.5 ether}(0);
+
+        (address sponsorAddress, uint sponsorAmount, uint pledgeId) = habit.sponsorList(0);
+        assertEq(sponsorAddress, sponsor);
+        assertEq(sponsorAmount, 0.5 ether);
+        assertEq(pledgeId, 0);
+    }
+
+    function testRedeemPledgeFullSuccess() public {
+        vm.prank(owner);
+        vm.deal(owner, 1 ether);
+        habit.createNewPledge{value: 1 ether}(30);
+
+        vm.prank(sponsor);
+        vm.deal(sponsor, 0.5 ether);
+        habit.sponsorPledge{value: 0.5 ether}(0);
+
+        vm.warp(block.timestamp + 31 days);
+
+        vm.prank(owner);
+        habit.redeemPledge(0, 30);
+
+        (, , , , , , bool isRedeemed) = habit.pledgeList(0);
+
+        assertEq(isRedeemed, true);
+        assertEq(address(habit).balance, 0);
+        assertEq(owner.balance, 1.5 ether);
+    }
+
+    function testRedeemPledgePartialSuccess() public {
+        // Set up the deployer address
+        deployer = address(0x3);
+        vm.prank(deployer);
+        habit = new HabitContract();
+        
+        vm.prank(owner);
+        vm.deal(owner, 1 ether);
+        habit.createNewPledge{value: 1 ether}(30);
+
+        vm.prank(sponsor);
+        vm.deal(sponsor, 0.5 ether);
+        habit.sponsorPledge{value: 0.5 ether}(0);
+
+        vm.warp(block.timestamp + 31 days);
+
+        vm.prank(owner);
+        habit.redeemPledge(0, 15);
+
+        (, , , , , , bool isRedeemed) = habit.pledgeList(0);
+        
+        assertEq(isRedeemed, true);
+        assertEq(owner.balance, 0.5 ether);
+        assertEq(sponsor.balance, 0.5 ether);
+        assertEq(deployer.balance, 0.5 ether);
+    }
+}

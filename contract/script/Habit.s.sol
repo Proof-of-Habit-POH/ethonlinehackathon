de54a6537c5576b8674f5e694c6d5abd9c787d44
Son <sitthavee.t@gmail.com>
2024-08-25 21:37:11 +0700

    create deploy script

--- contract/script/Habit.s.sol
+++ contract/script/Habit.s.sol
@@ -10,7 +10,8 @@ contract HabitScript is Script {
     function setUp() public {}
 
     function run() public {
-        vm.startBroadcast();
+        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+        vm.startBroadcast(deployerPrivateKey);
 
         habit = new HabitContract();
 

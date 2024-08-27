95df3aee33d3501d294c1a2e8b74ff4bf6918406
Son <sitthavee.t@gmail.com>
2024-08-27 22:06:43 +0700

    add env example

--- web/src/actions/index.ts
+++ web/src/actions/index.ts
@@ -1,14 +1,10 @@
-import { uploadImage } from "./upload-image";
 import { getHabitDetails } from "./get-habit-details";
 import { getEthPrice } from "./getEthPrice";
-import { verifyByAI } from "./verify-by-ai";
 import { verifyByGPT } from "./verify-by-gpt";
 import { getAllHabitsAndHabitTransactionsByWalletAddress } from "./get-habit-by-user";
 import { getSponsoringHabitsByWalletAddress } from "./get-sponsored-habit";
 export {
-  uploadImage,
   getHabitDetails,
-  verifyByAI,
   verifyByGPT,
   getEthPrice,
   getAllHabitsAndHabitTransactionsByWalletAddress,

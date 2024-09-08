4c31e78523aa3b1fbd01c1366aea3fcaf0e15c9a
Son <sitthavee.t@gmail.com>
2024-09-08 17:56:03 +0700

    clean code

--- web/prisma/resetDB.ts
+++
@@ -1,9 +0,0 @@
-require("dotenv").config();
-import prisma from "../src/db";
-
-async function run() {
-  await prisma.$executeRawUnsafe("DROP Database proofofhabit");
-  await prisma.$executeRawUnsafe("CREATE Database proofofhabit");
-}
-console.log("Reset DB..");
-run();
